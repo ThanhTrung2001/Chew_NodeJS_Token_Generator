6857331ce5c367f4bc2e3370889fb9cb6d94749f
Guinea <75150646+ThanhTrung2001@users.noreply.github.com>
2022-10-26 22:38:11 +0700

    use CSS and JS through EJS

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const configViewEngine = (app) => {
+    app.use(express.static('./src/public'))
     app.set("view engine", "ejs")  //cau hinh ejs la viewengine
     app.set("views", "./src/views") //mac dinh folder viet cac view
 }
